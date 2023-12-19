8306ac748c90c987b2891baac1468da163545e6c
ILS - Industrielogistik <marcel.jaworski@ils-gmbh.net>
2023-12-19 13:57:59 +0100

    fizzbuzz

--- directoryExist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-directory_name="test_directory"
-
-# # use "-d" to test if directory exist
-if [ -d "$directory_name" ]; then
-    echo "$directory_name exists as a directory"
-fi
---
+++ fizzBuzz.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/
+
+
+if (( $1%3 == 0 )); then
+	echo -n "Pling"
+fi
+if (( $1%5 == 0 )); then
+    echo -n "Plang"
+fi
+if (( $1%7 == 0 )); then
+    echo -n "Plong"
+fi
+if (( $1%3 != 0 && $1%5 != 0 && $1%7 != 0)); then
+	echo "$1"
+fi
