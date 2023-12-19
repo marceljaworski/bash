2c6d9b05c1a0d4002bef71299ac5b95d5f7daa69
ILS - Industrielogistik <marcel.jaworski@ils-gmbh.net>
2023-12-19 14:02:22 +0100

    fix

--- fizzBuzz.sh
+++ fizzBuzz.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/
+#!/bin/bash
 
 
 if (( $1%3 == 0 )); then
-	echo -n "Pling"
+	echo "Pling"
 fi
 if (( $1%5 == 0 )); then
-    echo -n "Plang"
+    echo "Plang"
 fi
 if (( $1%7 == 0 )); then
-    echo -n "Plong"
+    echo "Plong"
 fi
 if (( $1%3 != 0 && $1%5 != 0 && $1%7 != 0)); then
 	echo "$1"
